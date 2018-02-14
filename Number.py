d8eda49e840a6428ac43d1d2b297734a4be35370
Robert Osolnik <moj@email.com>
2018-02-14 20:12:42 +0100

    first commit

---
+++ Number.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+secret = 10
+guess = int(raw_input("Skrito število je: "))
+if guess == secret:
+    print "Bravo! Uganil si pravo število!"
+else:
+    print "Poskusi znova!"
+# Secret
+# Secret
+# Secret
